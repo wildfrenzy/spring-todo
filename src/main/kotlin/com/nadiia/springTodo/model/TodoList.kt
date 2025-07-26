c73a374ea79ecfb5442d86d5d8367c7fe4ae5b37
Esperanza <wildfrenzy0432@gmail.com>
2025-07-26 19:43:29 +0200

    prints -> exceptions

--- src/main/kotlin/com/nadiia/springTodo/model/TodoList.kt
+++ src/main/kotlin/com/nadiia/springTodo/model/TodoList.kt
@@ -8,23 +8,18 @@ data class TodoList(var name: String){
 
     private var _tasks: MutableList<Task> = mutableListOf()
 
-    init {
-        println("List created: [$_id]: $name")
-    }
-
     fun getTasks() = _tasks
     fun addTask(task: Task) {
         _tasks.add(task)
-        println("Task added: [${task.id}]: ${task.task}")
     }
     fun deleteTask(id: Int) {
         if (id < _tasks.size) {
             _tasks.removeAt(id)
-        } else println("Task with id $id doesn't exist")
+        } else throw NoSuchElementException("Task with id $id not found")
     }
     fun markDone(id: Int) {
         if (id < _tasks.size) {
             _tasks[id].markDone()
-        } else println("Task with id $id doesn't exist")
+        } else throw NoSuchElementException("Task with id $id not found")
     }
 }
