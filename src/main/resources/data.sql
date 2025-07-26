90550d99150f6bee6c5a8e45177c5ae8e70d9a1d
Esperanza <wildfrenzy0432@gmail.com>
2025-07-27 01:59:28 +0200

    feat: added DB setup and basic schema.sql data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO task (id, task, done) VALUES (1, 'My first task', FALSE);
+INSERT INTO task (id, task, done) VALUES (2, 'My another task', FALSE);
+INSERT INTO task (id, task, done) VALUES (4, 'My the other task', TRUE);
