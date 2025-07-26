9bbb64bb062735a273d53cb0d2e575b3c8cc1baf
Esperanza <wildfrenzy0432@gmail.com>
2025-07-27 00:15:41 +0200

    feat(app.js): added Delete task button next to each task

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -12,8 +12,19 @@ async function fetchTasks() {
         checkbox.checked = task.isDone;
         checkbox.onchange = () => toggleDone(task.id);
 
-        li.textContent = `${task.id}: ${task.task}`;
+        const span = document.createElement('span');
+        span.textContent = ` ${task.id}: ${task.task}`;
+
+        const delBtn = document.createElement('button');
+        delBtn.textContent = 'X';
+        delBtn.style.marginLeft = '10px';
+        delBtn.style.color = 'red';
+        delBtn.onclick = () => deleteTask(task.id);
+
+        li.appendChild(delBtn);
         li.appendChild(checkbox)
+        li.appendChild(span);
+
         list.appendChild(li);
     });
 }
@@ -22,7 +33,15 @@ async function toggleDone(id) {
     await fetch(`/api/task/${id}`, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id: id})
+        body: JSON.stringify({ id: id })
+    });
+    fetchTasks();
+}
+async function deleteTask(id) {
+    await fetch(`/api/task/${id}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: id })
     });
     fetchTasks();
 }
@@ -34,7 +53,6 @@ async function addTask() {
         alert('Please enter a task.');
         return;
     }
-
     await fetch('/api/task', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
